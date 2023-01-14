de7e7adfcf49326b71a3e83c32e88ed40370d912
asafworld <gabrielasaf10@gmail.com>
2023-01-14 20:39:39 -0300

    Define a estilização da página de Login (responsivo)

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import menuContext from "../../ContextAPI/MenuContext";
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
 
 function Login() {
   const {
     email,
     setEmail,
     password,
-    setPassword } = React.useContext(menuContext);
+    setPass } = React.useContext(menuContext);
 
   return(
     <article className="login-page">
@@ -17,24 +19,27 @@ function Login() {
       </section>
       <section className="login-form-section">
         <form>
-          <Form.Group className="input-group mb3">
+          <Form.Group className="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">e-Mail</span>
             <Form.Control
               type="text"
-              className="form-control"
+              className="form-control login-email-input"
               value={ email }
               onChange={ ({ target }) => setEmail(target.value)}
             />
           </Form.Group>
-          <Form.Group className="input-group mb3">
+          <Form.Group className="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Senha</span>
             <Form.Control
               type="password"
-              className="form-control"
-              value={ email }
-              onChange={ ({ target }) => setEmail(target.value)}
+              className="form-control login-password-input"
+              value={ password }
+              onChange={ ({ target }) => setPass(target.value)}
             />
           </Form.Group>
+          <Button type="submit">
+            Log-in
+          </Button>
         </form>
       </section>
     </article>
