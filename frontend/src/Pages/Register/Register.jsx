644584c81c97a36ce76f3d3c9ebbfac97f5bbad0
asafworld <gabrielasaf10@gmail.com>
2023-01-15 14:27:27 -0300

    Página de registro estilizada.

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -24,40 +24,58 @@ function Register() {
         />
         <h4>Uma casa para os seus menus!</h4>
       </section>
-      <section className="register-form-section">
-        <form>
-        <Form.Group className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">Usuário</span>
-            <Form.Control
-              type="text"
-              className="form-control register-username-input"
-              value={ username }
-              onChange={ ({ target }) => setUser(target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">e-Mail</span>
-            <Form.Control
-              type="text"
-              className="form-control register-email-input"
-              value={ email }
-              onChange={ ({ target }) => setEmail(target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">Senha</span>
-            <Form.Control
-              type="password"
-              className="form-control register-password-input"
-              value={ password }
-              onChange={ ({ target }) => setPass(target.value)}
-            />
-          </Form.Group>
-          <Button type="submit">
-            Registre-se
-          </Button>
-        </form>
-      </section>
+      <div>
+        <section className="register-form-section">
+          <form>
+            <Form.Group className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Usuário</span>
+              <Form.Control
+                type="text"
+                className="form-control register-username-input"
+                placeholder="meuUsuário"
+                value={ username }
+                onChange={ ({ target }) => setUser(target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">e-Mail</span>
+              <Form.Control
+                type="text"
+                className="form-control register-email-input"
+                placeholder="seu-email@email.com"
+                value={ email }
+                onChange={ ({ target }) => setEmail(target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Senha</span>
+              <Form.Control
+                type="password"
+                className="form-control register-password-input"
+                placeholder="******"
+                value={ password }
+                onChange={ ({ target }) => setPass(target.value)}
+              />
+            </Form.Group>
+            <Button
+              type="submit"
+              className="register-button"
+            >
+              Registre-se
+            </Button>
+          </form>
+        </section>
+        <section className="register-instructions-section">
+          <ul>
+            <li>
+              Nome de usuário deve conter no mín. 5 letras!
+            </li>
+            <li>
+              Senha de mín. de 6 caracteres, 1 letra maiúscula e 1 número!
+            </li>
+          </ul>
+        </section>
+      </div>
     </article>
   )
 }
