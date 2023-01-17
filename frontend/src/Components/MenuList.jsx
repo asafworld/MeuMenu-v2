63edc951dec9ee3f182e93c4c702a0af29ab268f
asafworld <gabrielasaf10@gmail.com>
2023-01-17 15:34:31 -0300

    Cria o componente de listagem de menus do cliente

---
+++ frontend/src/Components/MenuList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MenuList() {
+  return(
+    <section>
+      <ul>
+          <li>Menu 1 </li>
+          <li>Menu 2 </li>
+          <li>Menu 3 </li>
+      </ul>
+    </section>
+  )
+}
+
+export default MenuList;
