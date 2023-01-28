413f14f46b37b26193b467827a14f626c059b492
asafworld <gabrielasaf10@gmail.com>
2023-01-28 18:58:03 -0300

    [STYLE] Reorganazição de arquivos.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from '../src/Pages/Login/Login';
 import Register from './Pages/Register/Register';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Login /> } />
         <Route path='/register' element={ <Register /> } />
+        <Route path='/home' element={ <Home /> } />
       </Routes>
     </div>
   );
